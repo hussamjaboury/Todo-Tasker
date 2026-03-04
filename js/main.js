c671df36afd2a70713c520e9166d6ae0a81b2ae1
hussamjaboury <hussnajaborj9@gmail.com>
2026-03-04 11:18:57 +0300

    Prepare before adding add task modal

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const navbar = document.querySelector("[data-navbar]");
 
 document.addEventListener("click", (e) => {
   const navToggler = e.target.closest("[data-nav-toggler]");
-  console.log(navToggler);
   if (navToggler) toggleElements([overlay, navbar]);
 });
 
@@ -13,3 +12,24 @@ const toggleElements = (e) => {
     e.classList.toggle("active");
   });
 };
+
+// custom select
+const customSelect = document.querySelector("[data-custom-select]");
+const selectList = customSelect.querySelector("[data-select-list]");
+const arrow = document.querySelector("[data-select-arrow]");
+const result = document.querySelector(".custom-select .result");
+
+customSelect.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      console.log(e.srcElement.childNodes);
+      result.childNodes[0].replaceWith(e.target.textContent);
+    }
+    console.log(e.target);
+    selectList.classList.toggle("scale-y-0");
+    toggleElements([arrow]);
+    e.stopPropagation();
+  },
+  true,
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  darkMode: "class",
   content: ["./index.html"],
   theme: {
     extend: {
       colors: {
+        background_dark: "rgb(28 16 34 )",
+        background_light: "rgb(247 245 248)",
         licorice: "#281811ff",
         caf_noir: "#4f3120ff",
+        primary: "#a824ea",
         Sunset_Ashes: "#68606C",
         Vivid_Violet: "#AF25F4",
-        Deep_Midnight: "#1D1122",
+        Deep_Midnight: "rgba(28, 16, 34, 0.85)",
+        Soft_Lavender: "#c8b0e0ff",
+        white_2: "rgba(255, 255, 255, 0.02)",
         white_5: " rgba(255, 255, 255, 0.05);",
         white_10: "rgba(255,255,255,0.1)",
+        white_20: "rgba(255,255,255,0.2)",
         white_30: " rgba(255, 255, 255, 0.3);",
+        white_40: " rgba(255, 255, 255, 0.4);",
         papaya_whip: "#fff3dcff",
         alabaster: "#f2efe3ff",
         eerie_black: "#242424ff",
         jet: "#333333ff",
         amber_alpha: "#fef3c717",
         red: "#ff1b1b",
+        red_500: "rgba(239, 68, 68)",
+        red_400: "rgba(248, 113, 113)",
+        yellow_500: "rgba(253, 224, 71)",
+        yellow_400: "rgba(253, 224, 71)",
+        green_500: "rgba(34, 197, 94)",
+        green_400: "rgba(34, 197, 94)",
       },
       boxShadow: {
         shadowDark: "0 0 35px 0 #281811ff",
